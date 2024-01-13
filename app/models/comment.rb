d9d931504c0f30ed671aaf787c8746a0fb479646
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-13 22:36:07 +0500

    changings as per reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   after_save :update_comment_counter
 
   def update_comment_counter
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   after_save :update_like_counter
 
   # A method that updates the likes counter for a post.
