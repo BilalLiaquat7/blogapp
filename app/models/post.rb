96bab0f0ab684fb0b21544f60827d4d7fd708d9f
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-13 23:41:29 +0500

    validation added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   after_save :update_post_counter
   def recent_comments_return
     comments.order(created_at: desc).include(author).limit(5)
