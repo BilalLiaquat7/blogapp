274413465f8967b7cd9db5a68f9be6149cb97d54
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-14 01:00:48 +0500

    some fixes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
   after_save :update_like_counter
 
   # A method that updates the likes counter for a post.
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_post_counter
   def recent_comments_return
