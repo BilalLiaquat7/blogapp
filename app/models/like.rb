c5d90a6de26cdf8258411ef4a82830a624cabe2a
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-13 19:15:29 +0500

    processing data

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :update_like_counter
+
+  # A method that updates the likes counter for a post.
+  def update_like_counter
+    likes.increment!(:likesCounter)
+  end
+
 end
