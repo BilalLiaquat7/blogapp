43e555ce94dab08c054ea1dc059fbc67ed8ed3d9
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-14 00:55:35 +0500

    some fixing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
