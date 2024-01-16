0bd3bc7bb27af2dd0cf319f95eb5262c16e433c3
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-16 23:52:49 +0500

    view files are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index, :show] do
-    resources :post, only: [:index, :new, :create, :show] do
-      resources :comments, only [:new, :create]
-      resources :likes, only [:create]
+  get '/users', to: 'users#index'
+  # resources :users, only: [:index, :show] do
+    # resources :post, only: [:index, :new, :create, :show] do
+    #   resources :comments, only [:new, :create]
+    #   resources :likes, only [:create]
 end
