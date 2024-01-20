b65aa8d05e8ccd5dc4021fec8ab614656a4aaf50
Muhammad Bilal Liaquat <bilal@cognitivehealthintl.com>
2024-01-20 14:21:14 +0500

    views functionality completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     # it'll show users
   end
+
+  def set_users
+    @set_users ||= User.includes(:posts).all
+  end
+
+  def set_user
+    @set_user ||= User.includes(:posts).find(params[:id])
+    @recent_posts = @set_user.recent_posts
+  end
 end
