ac95ecb257cb565248c400f3bfd3590f0b672fc9
bilalliaquat7 <bilal.liaquat7@gmail.com>
2024-01-23 23:36:40 +0500

    some changes in model of comments and likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
   after_save :update_comment_counter
 
+  private
+  
   def update_comment_counter
     post.increment!(:commentssCounter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
   after_save :update_like_counter
 
-  # A method that updates the likes counter for a post.
+  private
+
   def update_like_counter
     likes.increment!(:likesCounter)
   end
